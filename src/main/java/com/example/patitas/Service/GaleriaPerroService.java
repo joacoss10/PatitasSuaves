bb4e736ec5166d52d37c203d51f3cd594a4b19dc
joacoss10 <joacomorelli1@gmail.com>
2026-05-18 22:45:36 -0300

    ready 2.1

--- src/main/java/com/example/patitas/Service/GaleriaPerroService.java
+++ src/main/java/com/example/patitas/Service/GaleriaPerroService.java
@@ -49,7 +49,7 @@ public class GaleriaPerroService {
                 perro.getServicioRealizado(),
                 perro.getComentarios(),
                 // Construimos la URL dinámica apuntando al endpoint que crearemos abajo
-                "[https://patitassuaves-backend.onrender.com/auth/vistaGaleria/galeria/foto/](https://patitassuaves-backend.onrender.com/auth/vistaGaleria/galeria/foto/)" + perro.getId()
+                "https://patitassuaves-backend.onrender.com/auth/vistaGaleria/galeria/foto/" + perro.getId()
         )).collect(Collectors.toList());
     }
 
